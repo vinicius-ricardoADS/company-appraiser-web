bcfca3d670d5845ee5aa54535c1a987ba8d74451
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-08-27 11:46:49 -0300

    feat: added footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={classes.footer}>
+            <p className={classes.authorization}>© Todos os direitos reservados</p>
+        </footer>
+    )
+};
+
+export default Footer;
